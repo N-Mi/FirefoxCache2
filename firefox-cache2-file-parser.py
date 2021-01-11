7f6e4d8803c84385ed92a8333d0d69136d495ad6
Nicolas PICHON - BSO <nicolas.pichon@bsonetwork.com>
2021-01-11 23:53:09 +0100

    first try at extracting files from cache

--- firefox-cache2-file-parser.py
+++ firefox-cache2-file-parser.py
@@ -8,6 +8,7 @@ import csv
 argParser = argparse.ArgumentParser(description='Parse Firefox cache2 files in a directory or individually.')
 argParser.add_argument('-f', '--file', help='single cache2 file to parse')
 argParser.add_argument('-d', '--directory', help='directory with cache2 files to parse')
+argParser.add_argument('-s', '--save', help='Save files')
 argParser.add_argument('-o', '--output', help='CSV output file')
 args = argParser.parse_args()
 
@@ -21,13 +22,16 @@ def ParseCacheFile (parseFile):
     fileSize = os.path.getsize(parseFile.name)
     parseFile.seek(-4, os.SEEK_END)
     #print parseFile.tell()
-    #print fileSize
+    #print filesize
+    print "fileSize : {0} ".format(fileSize)
     metaStart = struct.unpack('>I', parseFile.read(4))[0]
     #print metaStart
+    print "metaStart : {0} ".format(metaStart)
     numHashChunks = metaStart / chunkSize
     if metaStart % chunkSize :
         numHashChunks += 1
     #print 4 + numHashChunks * 2
+    print "4 + numHashChunks * 2  =  {0} ".format(4 + numHashChunks * 2)
     parseFile.seek(metaStart + 4 + numHashChunks * 2, os.SEEK_SET)
     #print parseFile.tell()
     version = struct.unpack('>I', parseFile.read(4))[0]
@@ -64,18 +68,31 @@ def ParseCacheFile (parseFile):
     print "key: {0}".format(key)
     print "key sha1: {0}\n".format(key_hash)
 
+    parseFile.seek(0, os.SEEK_SET)
+    data = parseFile.read(metaStart)
+    with open("/tmp/out", "wb") as data_out:
+        data_out.write(data)
+
 #ParseCacheFile(testFile)
 #procPath = script_dir + '/' + testDir
 if args.directory or args.file :
+    doCsv = False
     if args.output :
         doCsv = True
         csvFile = open(args.output, 'w')
         csvWriter = csv.writer(csvFile, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
         csvWriter.writerow(('Fetch Count', 'Last Fetch', 'Last Modified', 'Frecency', 'Expiration', 'Flags', 'URL', 'Key Hash'))
-    procPath = args.directory
-    fileList = os.listdir(procPath)
+
+    procPath = fileList = None
+    if args.directory:
+        procPath = args.directory
+        fileList = os.listdir(procPath)
+    elif args.file:
+        procPath = ""
+        fileList = [ args.file ]
+
     for filePath in fileList :
-        file = open(procPath + '/' + filePath, 'r')
+        file = open(procPath + '/' + filePath, 'rb')
         ParseCacheFile(file)
     if doCsv :
         print 'Data written to CSV file: {0}'.format(csvFile.name)
